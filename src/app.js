70998073d95976df03654dd1acb73adb3056ff06
Erik <lunamoranerik@gmail.com>
2023-09-10 19:23:34 -0300

    websocket y handlebars

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
 import express from "express";
+
 import handlebars from "express-handlebars";
+import { Server } from "socket.io";
+
+import ProductManager from "./manager/ProductManager.js";
+import CartManager from "./manager/CartManager.js";
+
+import productsRouter from "./routes/productsRouter.js";
+import cartsRouter from "./routes/cartsRouter.js";
+
+import viewsRouter from "./routes/viewsRouter.js";
 
 const app = express();
-const httpServer = app.listen(8080, () =>
-  console.log("servidor en funcionamiento")
-);
+const productManager = new ProductManager();
+const cartManager = new CartManager();
+const httpServer = app.listen(8080, () => console.log("Servidor encendido"));
+const socketServer = new Server(httpServer);
 
-app.engine("handlebars", "./src/views"); //vamos a utilizar handlebars como motor de plantillas
-app.set("views", "./srv/views");
+//HANDLEBARS
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
 app.set("view engine", "handlebars");
-app.use(express.static("./srv/public"));
+app.use(express.static("./srcpublic"));
+
+//nuevo midleWar
+app.use((req, res, next) => {
+  req.context = { socketServer };
+  next();
+});
+
+//Para que nuestra API lea JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/realtimeproducts/", viewsRouter);
+
+app.use("/api/products", productsRouter(productManager));
+app.use("/api/carts", cartsRouter(cartManager));
+
+/* app.listen(8080, () => console.log("Servidor encendido")); */
